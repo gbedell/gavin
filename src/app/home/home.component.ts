12f31f8a882511b15f8adf651e1ea94246fd88a4
gavin.bedell@gmail.com <gavinbedell@gavins-mbp.attlocal.net>
2018-07-05 22:31:38 -0500

    Made the homepage with image and social links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Social } from '../../models/social.model';
 
 @Component({
   selector: 'gavin-home',
@@ -11,6 +12,28 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  socialLinks: Social[] = [
+    {
+      "name": "LinkedIn",
+      "url": "https://www.linkedin.com/in/gavinbedell/",
+      "imageUrl": "/assets/social-icons/linkedin-34px.png"
+    },
+    {
+      "name": "GitHub",
+      "url": "https://github.com/gbedell",
+      "imageUrl": "/assets/social-icons/GitHub-Mark-32px.png"
+    },
+    {
+      "name": "Facebook",
+      "url": "https://www.facebook.com/gavin.bedell",
+      "imageUrl": "/assets/social-icons/f-ogo_RGB_HEX-58.png"
+    }
+  ]
+
+  goToSocial(url: string) {
+    window.open(url, "_blank");
   }
 
 }
---
+++ src/models/social.model.ts
@@ -0,0 +1,5 @@
+export class Social {
+    name: String;
+    url: String;
+    imageUrl: String;
+}
